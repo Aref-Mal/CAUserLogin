c411a16cb3e42abced6d0f531ed2e3ff16d4215d
Aref-Mal <aref.malekanian@mail.utoronto.ca>
2024-10-20 20:53:13 -0400

    Task 2.1

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -27,4 +27,9 @@ public interface LoginUserDataAccessInterface {
      */
     User get(String username);
 
+    /**
+     * Sets the current user with the given name.
+     * @param name the name of the user to set the current user to
+     */
+    void setCurrentUser(String name);
 }
